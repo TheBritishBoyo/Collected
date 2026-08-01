e4f54381586e2eb7625070a7d159cfadac89c1e1
TheBritishBoyo <141440456+TheBritishBoyo@users.noreply.github.com>
2026-08-01 15:20:17 +0100

    Add vercel web analytics

--- Collected/src/App.jsx
+++ Collected/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToHash from "./components/ScrollToHash";
@@ -26,6 +27,7 @@ function App() {
         </Routes>
       </main>
       <Footer />
+      <Analytics />
     </>
   );
 }
